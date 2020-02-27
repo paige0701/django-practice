45582eb1fbd21e5c6c381f2c8747b8a586a16ecb
Paige <paigechoi0701@gmail.com>
2020-02-27 20:36:18 +0900

    serialize query set

--- polls/views.py
+++ polls/views.py
@@ -1,12 +1,13 @@
+from django.core import serializers
 from django.http import HttpResponse
 
 from polls.models import Question
 
 
 def index(request):
-    latest_question_list = Question.objects.order_by('-pub_date')[:5]
-    output = ', '.join([q.question_text for q in latest_question_list])
-    return HttpResponse(output)
+    latest_question_list = Question.objects.filter().order_by('-pub_date')[:5]
+    data = serializers.serialize('json', latest_question_list, )
+    return HttpResponse(data)
 
 def detail(request, question_id):
     return HttpResponse('You are looking at question %s. '% question_id)
